5f87ac4c861eced902d951f2b0b5761b07246fb8
troldefar <r_emil@live.dk>
2023-10-29 13:34:40 +0100

    first like

---
+++ cpu.js
@@ -0,0 +1,99 @@
+const createMemory = require('./createMemory');
+const semantics    = require('./semantics');
+
+class CPU {
+    constructor(memory) {
+        this.memory = memory;
+        this.registerNames = ['instructionPointer', 'accumulator', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8'];
+        this.registers = createMemory(this.registerNames.length * 2);
+        this.registerMap = this.registerNames.reduce((map, name, i) => {
+            map[name] = i * 2;
+            return map;
+        }, {});
+    }
+
+    log() {
+        this.registerNames.forEach(name => {
+            console.log(`Name: ${name} - Value: 0x${this.getRegister(name).toString(16).padStart(4, '0')}`);
+        });
+    }
+
+    inspect(address) {
+        const nextBytes = Array.from({length: 8}, (_, i) => 
+            this.memory.getUint8(address + 1)
+        ).map(value => `0x${value.toString(16).padStart(2, '0')}`);
+
+        console.log(`0x${address.toString(16).padStart(4, '0')}: ${nextBytes.join(' ')}`);
+    }
+
+    getRegister(name) {
+        if (!(name in this.registerMap)) throw new Error('Invalid register');
+        return this.registers.getUint16(this.registerMap[name]);
+    }
+
+    setRegister(name, value) {
+        if (!(name in this.registerMap)) throw new Error('No such register');
+        return this.registers.setUint16(this.registerMap[name], value);
+    }
+
+    step() {
+        const instruction = this.fetch();
+        return this.execute(instruction);
+    }
+
+    fetch(bitSize = 8) {
+        const eightBit = bitSize === 8;
+        const nextInstruction = this.getRegister('instructionPointer');
+        const instruction     = eightBit ? this.memory.getUint8(nextInstruction) : this.memory.getUint16(nextInstruction);
+        this.setRegister('instructionPointer', nextInstruction + (eightBit ? 1 : 2));
+        return instruction;
+    }
+
+    getNextRegister() {
+        return (this.fetch() % this.registerNames.length) * 2;
+    }
+
+    execute(instruction) {
+        switch (instruction) {
+            case semantics.MOVE_LIT_REG: {
+                const literal = this.fetch(16);
+                const register  = this.getNextRegister();
+                this.registers.setUint16(register, literal);
+                return;
+            }
+            case semantics.MOVE_REG_REG: {
+                const registerFrom = this.getNextRegister();
+                const registerTo = this.getNextRegister();
+                const value = this.registers.getUint16(registerFrom);
+                this.registers.setUint16(registerTo, value);
+                return;
+            }
+            case semantics.MOVE_REG_MEM: {
+                const registerFrom = this.getNextRegister();
+                const address = this.fetch(16);
+                const value = this.registers.getUint16(registerFrom);
+                this.memory.setUint16(address, value);
+                return;
+            }
+            case semantics.MOVE_MEM_REG: {
+                const address = this.fetch(16);
+                const registerTo = this.getNextRegister();
+                const value = this.memory.getUint16(address);
+                this.registers.setUint16(registerTo, value);
+                return;
+            }
+            case semantics.ADD_REG: {
+                const r1 = this.fetch();
+                const r2 = this.fetch();
+                const registerValue1 = this.registers.getUint16(r1 * 2);
+                const registerValue2 = this.registers.getUint16(r2 * 2);
+                this.setRegister('accumulator', registerValue1 + registerValue2);
+                return;
+            }
+            default:
+                break;
+        }
+    }
+}
+
+module.exports = CPU;
---
+++ index.js
@@ -0,0 +1,43 @@
+const createMemory = require('./createMemory');
+const CPU          = require('./cpu');
+const semantics    = require('./semantics');
+
+const memory = createMemory(256*256);
+const writeableBytes = new Uint8Array(memory.buffer);
+
+const cpu = new CPU(memory);
+
+let i = 0;
+
+writeableBytes[i++] = semantics.MOVE_LIT_REG;
+writeableBytes[i++] = 0x12;
+writeableBytes[i++] = 0x34;
+writeableBytes[i++] = semantics.globals.R1;
+
+writeableBytes[i++] = semantics.MOVE_LIT_REG;
+writeableBytes[i++] = 0xAB;
+writeableBytes[i++] = 0xCD;
+writeableBytes[i++] = semantics.globals.R2;
+
+writeableBytes[i++] = semantics.ADD_REG_REG;
+writeableBytes[i++] = semantics.globals.R1;
+writeableBytes[i++] = semantics.globals.R2;
+
+writeableBytes[i++] = semantics.MOVE_REG_MEM;
+writeableBytes[i++] = semantics.globals.ACC;
+writeableBytes[i++] = 0x01;
+writeableBytes[i++] = 0x00;
+
+cpu.inspect(cpu.getRegister('instructionPointer'));
+cpu.inspect(0x0100);
+
+for(let i = 0; i < 4; i++) {
+    cpu.step();
+    cpu.log();
+    cpu.inspect(cpu.getRegister('instructionPointer'));
+    cpu.inspect(0x0100);
+}
+
+cpu.log();
+cpu.inspect(cpu.getRegister('instructionPointer'));
+cpu.inspect(0x0100);
