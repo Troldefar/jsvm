a23353e30f6666a1af8549d47d7edcba7f5e00b6
troldefar <r_emil@live.dk>
2023-11-04 08:49:38 +0100

    write to console test

--- fakeDevice.js
+++ fakeDevice.js
@@ -14,15 +14,19 @@ const moveTo = (x, y) => {
     process.stdout.write(`\x1b[${y};${x}H`);
 }
 
+const checkCmd = cmd => {
+    if (cmd === 0xff) clearScreen();
+    else if (cmd === 0x01) setBold();
+    else if (cmd === 0x02) setRegular();
+}
+
 const fakeDevice = () => {
     return {
         getUint: (byte = 8) => 0,
         setUint: (address, data) => {
             const cmd = (data & 0xff00) >> 8;
             const characterValue = data & 0x00ff;
-            if (cmd === 0xff) clearScreen();
-            else if (cmd === 0x01) setBold();
-            else if (cmd === 0x02) setRegular();
+            checkCmd(cmd);
             const x = (address % 16) + 1;
             const y = Math.floor(address / 16) + 1;
             moveTo(x * 2, y);
